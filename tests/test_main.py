7d039eacf993fa44ca50451aead518f50e8ac8d0
wenleicao <wenleicao@hotmail.com>
2026-06-06 23:19:07 -0400

    fix import pytest

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,13 +2,14 @@ import pytest
 import sys
 sys.path.append(r"D:\cicd\app")  # Add the app directory to the system path
 from main import app
+#print(app.__doc__)
 
-# @pytest.fixture
-# def client():
-#     with app.test_client() as client:
-#         yield client
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
 
-# def test_index_endpoint(client):
-#     response = client.get("/")
-#     assert response.status_code == 200
-#     assert response.json["status"] == "healthy"
+def test_index_endpoint(client):
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json["status"] == "healthy"
